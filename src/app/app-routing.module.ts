fe41f7c4dec2be4921342532cfd09f6960638beb
EXSR <eder-x@hotmail.com>
2022-02-28 17:00:58 -0300

    criado projeto album online, tema 1 cloud-fade

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { Error404Component } from './modules/error/error404/error404.component';
+import { MainHomeComponent } from './modules/home/main/main-home.component';
+
+const routes: Routes = [{
+  path: '',
+  component: MainHomeComponent,  
+},
+{
+  path: '**',
+  component: Error404Component
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
